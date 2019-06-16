62d38b6e72e24dadebaed9ce924bbe73cd220cde
achievec <achievec@gmail.com>
2019-06-16 14:39:51 +0800

    add learning-node folder

---
+++ learning-node/echo_server_pipe.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+http.createServer((request, response) => {
+    if (request.method === 'POST' && request.url === '/echo') {
+        request.pipe(response);
+    } else {
+        response.statusCode = 404;
+        response.end();
+    }
+}).listen(8080);
